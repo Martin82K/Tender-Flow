281e38512df13a2a61171745a4d56720c743e93f
Martin82K <martinkalkus82@gmail.com>
2026-07-13 17:50:35 +0200

    release version 1.8.14

--- features/whats-new/useWhatsNew.ts
+++ features/whats-new/useWhatsNew.ts
@@ -2,7 +2,7 @@ import { useState, useCallback } from "react";
 import { APP_VERSION } from "@/config/version";
 
 const STORAGE_KEY = "tf_whatsNew_lastSeen";
-const SKIPPED_WHATS_NEW_VERSIONS = new Set(["1.7.0", "1.7.2", "1.8.0", "1.8.4", "1.8.5", "1.8.6", "1.8.7", "1.8.8", "1.8.9", "1.8.10", "1.8.11", "1.8.12", "1.8.13"]);
+const SKIPPED_WHATS_NEW_VERSIONS = new Set(["1.7.0", "1.7.2", "1.8.0", "1.8.4", "1.8.5", "1.8.6", "1.8.7", "1.8.8", "1.8.9", "1.8.10", "1.8.11", "1.8.12", "1.8.13", "1.8.14"]);
 
 export const shouldShowWhatsNew = (
   appVersion: string,
